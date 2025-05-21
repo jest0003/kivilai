dac2174376495b19fe081bde50741ff10aa25354
Kamilla <kach0006@stud.kea.dk>
2025-05-21 13:28:53 +0200

    JS som får produktbilledet til at vises forskelligt afhængigt af skærmstørrelsen

--- produktside.js
+++ produktside.js
@@ -114,21 +114,29 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  const slides = document.getElementsByClassName("mySlides");
+
+  if (window.innerWidth >= 600) {
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "block";
+    }
+    return;
+  }
+
   if (n > slides.length) {
     slideIndex = 1;
   }
   if (n < 1) {
     slideIndex = slides.length;
   }
-  for (i = 0; i < slides.length; i++) {
+
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  /* for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  } */
+
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
 }
+
+window.addEventListener("resize", () => {
+  showSlides(slideIndex);
+});
